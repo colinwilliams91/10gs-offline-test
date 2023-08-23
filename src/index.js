b8d51e14f12977b42a90e9d131382c2b8187d4ad
colin <colin.williams.dev@gmail.com>
2023-08-22 23:29:10 -0500

    fix: solved

--- src/index.js
+++ src/index.js
@@ -26,34 +26,31 @@
  */
 var rand = function () { return Math.floor(Math.random() * (200 - 100 + 1) + 100); };
 var replaceTubesAndSort = function (tubeUnit) { return tubeUnit.map(function (_) { return rand(); }).sort(function (a, b) { return a - b; }); };
-// sorts classRoom Matrix by shortest second item (Tube) to longest (array[1])
+// sorts classRoom Matrix by shortest second item (Tube "lifetime") to longest (TubeUnit[1])
 var sortClassroom = function (classroom) { return classroom.sort(function (tubeUnitOne, tubeUnitTwo) { return tubeUnitOne[1] - tubeUnitTwo[1]; }); };
 /**
  * Givens
  */
 var universityClassroom = Array.from({ length: 4 }, function () { return replaceTubesAndSort([0, 0, 0, 0]); });
-var tubesAndCost = { tubes: 16, cost: 112 };
+var tubesAndCost = { tubes: 0, cost: 112 }; /* <<-- this accounts for initial 16 tubes cost */
 var yearInHours = 2700;
 /**
  * Simulation
  */
 var computeTubesBrokenAndCosts = function (classroom, runTimeHours, output) {
-    // sort classroom by second item unit[1] of each unit, these will be the determining tubes as they will force unit replacement
+    // sort classroom by second item (unit[1]) of each unit, these will be the determining tubes as they will force unit (4 tubes) replacement
     var sortedClassroom = sortClassroom(classroom);
     while (runTimeHours > 0) {
-        sortedClassroom.forEach(function (unit, i, classroom) {
-            console.log("CHECK:", output, sortedClassroom, runTimeHours);
-            unit[0]--;
-            unit[1]--;
-            unit[2]--;
-            unit[3]--;
+        sortedClassroom.forEach(function (unit, i) {
+            // using bracket notation inside forEach to modify original array in place ("spends" 1 hour for each child Tube inside TubeUnit)
+            unit.forEach(function (_, j, unit) { return unit[j]--; });
             if (unit[0] === 0 && unit[1] >= 0) {
-                // count single tube breaking
+                // count single tube breaking (tubes broken += 1, no cost increase yet)
                 output.tubes++;
             }
             if (unit[0] < 1 && unit[1] === 0) {
                 sortedClassroom[i] = replaceTubesAndSort(unit);
-                // count second tube breaking, triggering 4 tube replacements (cost += 7 * 4 && tubes += 2 total)
+                // count second tube breaking, triggering 4 tube replacements (tubes broken += 1 && cost += 7 * 4)
                 output.tubes++;
                 output.cost += 7 * 4;
             }
--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ const rand = (): number => Math.floor(Math.random() * (200 - 100 + 1) + 100);
 
 const replaceTubesAndSort = (tubeUnit: TubeUnit): TubeUnit => tubeUnit.map((_: number) => rand()).sort((a: number, b: number) => a - b);
 
-// sorts classRoom Matrix by shortest second item (Tube) to longest (array[1])
+// sorts classRoom Matrix by shortest second item (Tube "lifetime") to longest (TubeUnit[1])
 const sortClassroom = (classroom: Classroom): Classroom => classroom.sort((tubeUnitOne: TubeUnit, tubeUnitTwo: TubeUnit) => tubeUnitOne[1] - tubeUnitTwo[1]);
 
 /**
@@ -39,7 +39,7 @@ const sortClassroom = (classroom: Classroom): Classroom => classroom.sort((tubeU
 
 const universityClassroom: Classroom = (Array as any).from({ length: 4 }, () => replaceTubesAndSort([0, 0, 0, 0]));
 
-const tubesAndCost: TubesAndCost = { tubes: 16, cost: 112 };
+const tubesAndCost: TubesAndCost = { tubes: 0, cost: 112 }; /* <<-- this accounts for initial 16 tubes cost */
 
 let yearInHours: number = 2700;
 
@@ -58,27 +58,26 @@ type TubesAndCost = { "tubes": number; "cost": number; };
  */
 
 const computeTubesBrokenAndCosts = (classroom: Classroom, runTimeHours: number, output: TubesAndCost): TubesAndCost => {
-  // sort classroom by second item unit[1] of each unit, these will be the determining tubes as they will force unit replacement
+  // sort classroom by second item (unit[1]) of each unit, these will be the determining tubes as they will force unit (4 tubes) replacement
   let sortedClassroom: TubeUnit[] = sortClassroom(classroom);
 
   while (runTimeHours > 0) {
-    sortedClassroom.forEach((unit: TubeUnit, i: number, classroom: Classroom) => {
-      console.log("CHECK:", output, sortedClassroom, runTimeHours);
-      unit[0]--;
-      unit[1]--;
-      unit[2]--;
-      unit[3]--;
+    sortedClassroom.forEach((unit: TubeUnit, i: number) => {
+      // using bracket notation inside forEach to modify original array in place ("spends" 1 hour for each child Tube inside TubeUnit)
+      unit.forEach((_: number, j: number, unit: TubeUnit) => unit[j]--);
+
       if (unit[0] === 0 && unit[1] >= 0) {
-        // count single tube breaking
+        // count single tube breaking (tubes broken += 1, no cost increase yet)
         output.tubes++;
       }
       if (unit[0] < 1 && unit[1] === 0) {
-        sortedClassroom[i] = replaceTubesAndSort(unit);
-        // count second tube breaking, triggering 4 tube replacements (cost += 7 * 4 && tubes += 2 total)
+        // count second tube breaking, triggering 4 tube replacements (tubes broken += 1 && cost += 7 * 4)
         output.tubes++;
         output.cost += 7 * 4;
+        sortedClassroom[i] = replaceTubesAndSort(unit);
       }
     });
+    // all 16 tubes should degrade 1 hour per 1 runTimeHour
     runTimeHours--;
   }
   console.log(output);
